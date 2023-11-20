5282b4124d4ed8ceb25db98bbc6ce87d23eba0d3
Cedric Wiese <cedric03082000@gmail.com>
2023-11-21 00:45:41 +0100

    added new nvim

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+require("cedricwiese.remap")
+require("cedricwiese.set")
+
+vim.api.nvim_set_keymap('n', 'j', 'h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'k', 'j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'l', 'k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ';', 'l', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('v', 'j', 'h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'k', 'j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'l', 'k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', ';', 'l', { noremap = true, silent = true })
